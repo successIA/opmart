aa8775ec52a005dc4fcebc4ee09daec22e3b001d
Ibrahim Abass <35644457+successIA@users.noreply.github.com>
2021-12-29 09:25:40 +0100

    Refactor listing image api to accept only one at a time

--- backend/opmart/listings/api.py
+++ backend/opmart/listings/api.py
@@ -1,6 +1,7 @@
 from rest_framework import mixins, status, viewsets
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.permissions import (
+    AllowAny,
     IsAuthenticated,
     IsAuthenticatedOrReadOnly,
 )
@@ -18,22 +19,22 @@ from opmart.listings.serializers import (
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.order_by("name")
     serializer_class = CategorySerializer
+    permission_classes = [AllowAny]
 
 
 class ListingViewSet(
-    mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
 ):
-    queryset = Listing.objects.order_by("-created_at")
+    queryset = Listing.objects.prefetch_related("images").order_by("-created_at")
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_serializer_class(self):
         if self.action == "create":
             return ListingCreateSerializer
         return ListingSerializer
-
-        # def perform_create(self, serializer):
-        #     # return super().perform_create(serializer)
-        #     pass
 
 
 class ListingImageViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
@@ -42,15 +43,5 @@ class ListingImageViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     permission_classes = [IsAuthenticated]
     parser_classes = [MultiPartParser, FormParser]
 
-    def create(self, request, *args, **kwargs):
-        serializer = ListingImageSerializer(
-            data=request.data, context={"request": request}
-        )
-        serializer.is_valid(raise_exception=True)
-
-        listing_image_ids = serializer.save()
-
-        data = {
-            "listing_images": listing_image_ids,
-        }
-        return Response(data, status=status.HTTP_201_CREATED)
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
--- backend/opmart/listings/serializers.py
+++ backend/opmart/listings/serializers.py
@@ -80,31 +80,14 @@ class ListingCreateSerializer(serializers.ModelSerializer):
         return listing
 
 
-class ListingImageSerializer(serializers.Serializer):
-    images = serializers.ListField(child=serializers.ImageField(), allow_empty=False)
-
-    def validate_images(self, value):
-        if len(value) > MAX_IMAGE_COUNT:
-            msg = _("You cannot upload more than 10 images.")
+class ListingImageSerializer(serializers.ModelSerializer):
+    def validate_image(self, value):
+        if value.size > MAX_IMAGE_SIZE:
+            msg = _("Image size cannot be greater than 5MB")
             raise serializers.ValidationError(msg)
-
-        for image in value:
-            if image.size > MAX_IMAGE_SIZE:
-                msg = _("Maximum size of a single image cannot be greater 5MB")
-                raise serializers.ValidationError(msg)
         return value
 
     class Meta:
         model = ListingImage
-        fields = ["images"]
-
-    @transaction.atomic
-    def save(self):
-        request = self.context["request"]
-
-        listing_image_ids = []
-        for image in self.validated_data["images"]:
-            listing_image = ListingImage.objects.create(owner=request.user, image=image)
-            listing_image_ids.append(listing_image.pk)
-
-        return listing_image_ids
+        fields = ["id", "image"]
+        extra_kwargs = {"image": {"write_only": True}}
